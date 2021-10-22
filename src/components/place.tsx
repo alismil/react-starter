a01f59ba413924d148b856a6919f9af2359d7db9
Alisa Milchevskaya <alisa-milchevskaya@hotmail.co.uk>
2021-10-22 16:57:01 +0100

    format with prettier

--- src/components/place.tsx
+++ src/components/place.tsx
@@ -1,23 +1,26 @@
 import { parentPort } from "worker_threads";
 
 interface PlaceProps {
-    title: string;
-    place: string;
-    country: string;
-    imageURL: string;
-    googleMapsURL: string;
-    description: string;
-  }
-  
-  function Place(props: PlaceProps): JSX.Element {
-    return (
-        <>
-        <img src={props.imageURL} alt={props.place} />
-        <h2>{props.title}</h2>
-        <h3>{props.place}, {props.country} (<a href={props.googleMapsURL}>map link</a>)</h3>
-        <p>{props.description}</p>
-        </>
-    );
-  }
-  
-  export default Place;
+  title: string;
+  place: string;
+  country: string;
+  imageURL: string;
+  googleMapsURL: string;
+  description: string;
+}
+
+function Place(props: PlaceProps): JSX.Element {
+  return (
+    <>
+      <img src={props.imageURL} alt={props.place} />
+      <h2>{props.title}</h2>
+      <h3>
+        {props.place}, {props.country} (
+        <a href={props.googleMapsURL}>map link</a>)
+      </h3>
+      <p>{props.description}</p>
+    </>
+  );
+}
+
+export default Place;
